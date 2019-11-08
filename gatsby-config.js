1cec0efbca886de14a6e8d40f00faadf3acc664a
ehowey <johno-actions-push-subdirectories@example.org>
2019-11-08 06:02:47 +0000

    Update gatsby-starter-catalyst-basic from ehowey/gatsby-theme-catalyst

---
+++ gatsby-config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  siteMetadata: {
+    title: `Basic`,
+    description: `Speed up your Gatsby development workflow. Designed as an opinionated and high speed starter.`,
+    author: `Eric Howey`,
+    siteUrl: `https://gatsby-starter-catalyst-basic.netlify.com`, //Change to you site address, required for sitemap.xml and robots.txt file
+    pageLinks: [
+      {
+        name: `Page 1`,
+        link: `/page-1`,
+      },
+      {
+        name: `Page 2`,
+        link: `/page-2`,
+      },
+    ],
+    anchorLinks: [
+      {
+        name: `Anchor 1`,
+        link: `#anchor-1`,
+      },
+      {
+        name: `Anchor 2`,
+        link: `#anchor-2`,
+      },
+    ],
+    socialLinks: [
+      {
+        name: `Email`,
+        url: `eric@erichowey.dev`,
+        location: `footer`, //Options are "all", "header", "footer"
+      },
+      {
+        name: `Github`,
+        url: `https://www.github.com/ehowey`,
+        location: `all`, //Options are "all", "header", "footer"
+      },
+      {
+        name: `Twitter`,
+        url: `https://www.twitter.com/erchwy`,
+        location: `header`, //Options are "all", "header", "footer"
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-catalyst-core`,
+      options: {
+        displaySiteLogo: true,
+        displaySiteTitle: true,
+        invertSiteLogo: false,
+        mobileMenuBreakpoint: "768px",
+        navType: "default", // "default", "anchor", "blended"
+        headerPosition: "static", // "static" or "sticky" work best, "fixed" is possible
+      },
+    },
+    `gatsby-theme-catalyst-header-basic`,
+    `gatsby-theme-catalyst-footer-basic`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-catalyst-basic`,
+        short_name: `catalyst`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `content/assets/logo-512.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
